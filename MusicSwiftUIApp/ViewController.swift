b46ab73561ea6ce0fce00cd0cf7402f84f8a57de
Amrendra <amar.roy17@gmail.com>
2021-10-16 09:23:12 +0530

    adding local project to git repo
    Signed-off-by: Amrendra <amar.roy17@gmail.com>

---
+++ MusicSwiftUIApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MusicSwiftUIApp
+//
+//  Created by Amrendra on 16/10/21.
+//
+
+import UIKit
+import MusicCoreFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let loginReq = LoginRequest.loginRequestMethod()
+    }
+
+
+}
+
